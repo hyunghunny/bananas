44467ccc28955005b29152fba72b09735c4330bf
Hyunghun Cho <hyunghunny@gmail.com>
2020-10-27 18:15:13 +0900

    update run config

--- run_experiments_sequential.py
+++ run_experiments_sequential.py
@@ -121,8 +121,8 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Args for BANANAS experiments')
-    parser.add_argument('--trials', type=int, default=50, help='Number of trials')
-    parser.add_argument('--search_space', type=str, default='nasbench_201_cifar100', \
+    parser.add_argument('--trials', type=int, default=100, help='Number of trials')
+    parser.add_argument('--search_space', type=str, default='nasbench_201_imagenet', \
         help='nasbench or darts')
     parser.add_argument('--algo_params', type=str, default='bananas', help='which parameters to use')
     #parser.add_argument('--output_filename', type=str, default='round', help='name of output files')
